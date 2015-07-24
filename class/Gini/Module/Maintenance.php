c41b653f70e251ae7a5aeef4a549535db1a4ae43
冷凯 <lengkai@YangWangtekiMacBook-Pro-3.local>
2015-07-24 10:39:30 +0800

    修复gini cache不生效的bug

--- class/Gini/Module/Maintenance.php
+++ class/Gini/Module/Maintenance.php
@@ -9,13 +9,19 @@ class Maintenance
         //获取系统维护模块的状态信息
         $config = \Gini\Config::get('maintenance.default');
 
+        $route = \Gini\CGI::route();
+        $path = 'maintain';
+
         //维护模块开启
-        if($config['status'] && $config['status'] == 'on') {
-            $route = \Gini\CGI::route();
-            $path = 'maintain';
+        if ($config['status'] && $config['status'] == 'on') {
             //避免重定向跳转
-            if ($route != $path) {
+            if ($route && $route != $path) {
                 \Gini\CGI::redirect($path);
+            }
+        }else{
+            //维护模块关闭时访问维护页自动跳转到首页
+            if ($route == $path) {
+                \Gini\CGI::redirect('/');
             }
         }
     }
