9bc3b7f412d5abd795208fa623729a3eb37d0116
冷凯 <lengkai@YangWangtekiMacBook-Pro-3.local>
2015-07-27 17:30:22 +0800

    修改配置文件相关变量

--- class/Gini/Controller/CGI/Maintain.php
+++ class/Gini/Controller/CGI/Maintain.php
@@ -7,11 +7,13 @@ class Maintain extends \Gini\Controller\CGI
     public function __index()
     {
     	//获取系统维护模块的状态信息
-        $config = \Gini\Config::get('maintenance.default');
+        $config = \Gini\Config::get('maintenance');
 
         if ($config['status'] && $config['status'] == 'on') {
+            $view_vars = $config['view_vars'];
+
         	//维护模块开启时跳转到维护页面
-    		return \Gini\IoC::construct('\Gini\CGI\Response\HTML', V('maintenance', array('maintain_end_date' => $config['maintain_end_date'])));
+    		return \Gini\IoC::construct('\Gini\CGI\Response\HTML', V('maintenance', array('maintain_end_date' => $view_vars['maintain_end_date'])));
         } else {
         	//维护模块关闭时跳转到首页
         	return $this->redirect('/');
--- class/Gini/Module/Maintenance.php
+++ class/Gini/Module/Maintenance.php
@@ -7,14 +7,14 @@ class Maintenance
     public static function setup()
     {
         //获取系统维护模块的状态信息
-        $config = \Gini\Config::get('maintenance.default');
+        $config = \Gini\Config::get('maintenance');
 
         $route = \Gini\CGI::route();
         $path = 'maintain';
 
         //维护模块开启
         if ($config['status'] && $config['status'] == 'on') {
-            //判断$route是为了避免gini cache执行失败，判断$route != $path是为了避免重定向跳转
+            //判断$route是防止后续的模块不给他机会直接跳转，判断$route != $path是防止重定向跳转
             if ($route && $route != $path) {
                 \Gini\CGI::redirect($path);
             }
